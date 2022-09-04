ba764f4a2185db4446d2606f64618b8142251487
Mücahit Kökdemir <76880536+mucahitkok@users.noreply.github.com>
2022-09-04 19:31:40 +0300

    route config

---
+++ API/src/Router.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function Router() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
