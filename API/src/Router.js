f7ef2005c1d9e9e13da186655ffa6c46e8db095a
Mücahit Kökdemir <76880536+mucahitkok@users.noreply.github.com>
2022-09-04 20:27:57 +0300

    ..

--- API/src/Router.js
+++ API/src/Router.js
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
+import HomeScreen from './pages/Home';
 
 export default function Router() {
   return (
